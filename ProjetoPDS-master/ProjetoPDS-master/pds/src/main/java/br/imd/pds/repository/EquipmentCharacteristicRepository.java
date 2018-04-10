14f1dbbf6aaad7832866155d01b892351472da39
Vitor Hugo Barros <vitorbarros@A202-03L.imd.local>
2018-04-10 11:06:43 -0300

    Removendo pasta que Susanny adicionou sem querer

--- ProjetoPDS-master/ProjetoPDS-master/pds/src/main/java/br/imd/pds/repository/EquipmentCharacteristicRepository.java
+++
@@ -1,8 +0,0 @@
-package br.imd.pds.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.imd.pds.model.EquipmentCharacteristic;
-
-public interface EquipmentCharacteristicRepository extends JpaRepository<EquipmentCharacteristic,Long> {
-
-}
--- ProjetoPDS-master/ProjetoPDS-master/pds/src/main/java/br/imd/pds/repository/SectorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.imd.pds.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.imd.pds.model.Sector;
-
-public interface SectorRepository extends JpaRepository<Sector,Long> {
-	
-
-}
