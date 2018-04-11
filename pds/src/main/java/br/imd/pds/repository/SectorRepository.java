3acc39ff8eb994af63d5efdbfea43bd673841a5d
Vitor Hugo Barros <vitorbarros@A202-06L.imd.local>
2018-04-11 11:22:35 -0300

    Implementados os métodos insert, delete, search e update da classe ManagementAdministratorService

--- pds/src/main/java/br/imd/pds/repository/SectorRepository.java
+++ pds/src/main/java/br/imd/pds/repository/SectorRepository.java
@@ -1,9 +1,11 @@
 package br.imd.pds.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.imd.pds.model.Sector;
 
 public interface SectorRepository extends JpaRepository<Sector,Long> {
 	
-
+	@Query("select u from Sector u where u.name = ?1")
+	public Sector findByName(String name);
 }
