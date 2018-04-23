4f8adecc1d36c8595db6d20d67c06e312fe9c696
Vitor Hugo Barros <vitorbarros@A303-20L.imd.local>
2018-04-23 13:54:51 -0300

    Classe Role implementada

--- pds/src/test/java/br/imd/pds/DAO/UserRepositoryTest.java
+++ pds/src/test/java/br/imd/pds/DAO/UserRepositoryTest.java
@@ -15,12 +15,12 @@ public class UserRepositoryTest {
 	@Test
 	public void testRepository() {
 		
-		User user01 = new User("78945612302","Pedro","pedro@asd.com");
-		User user02 = new User("12345678925","João","joao@a.rsyt");
-		User user03 = new User("45678913252","Maria","maria@rrr.rgsd");
-	
-		repository.save(user01);
-		repository.save(user02);
-		repository.save(user03);
+//		User user01 = new User("78945612302","Pedro","pedro@asd.com");
+//		User user02 = new User("12345678925","João","joao@a.rsyt");
+//		User user03 = new User("45678913252","Maria","maria@rrr.rgsd");
+//	
+//		repository.save(user01);
+//		repository.save(user02);
+//		repository.save(user03);
 	}
 }
