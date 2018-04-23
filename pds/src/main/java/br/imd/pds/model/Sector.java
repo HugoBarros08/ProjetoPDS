d75c920eaa8c514b733a61592741dcb2f4c30715
Vitor Hugo Barros <vitorbarros@A202-07L.imd.local>
2018-04-23 15:54:20 -0300

    removendo impotações não usadas

--- pds/src/main/java/br/imd/pds/model/Sector.java
+++ pds/src/main/java/br/imd/pds/model/Sector.java
@@ -1,14 +1,11 @@
 package br.imd.pds.model;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 
 /**
