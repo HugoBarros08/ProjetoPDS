832e13d0e89033cb5dd84f1e257efe9fc27305c9
Vitor Hugo Barros <vitorbarros@A202-11L.imd.local>
2018-04-24 17:31:03 -0300

    Classe para o envio de notificações criada

---
+++ pds/src/main/java/br/imd/pds/service/NotificationEmailService.java
@@ -0,0 +1,31 @@
+package br.imd.pds.service;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import br.imd.pds.model.Equipment;
+
+public class NotificationEmailService{
+	
+	@Autowired
+    public JavaMailSender emailSender;
+	
+	public static final String subject = "Lembrete de manutenção";
+ 
+    public void sendSimpleMessage(String to, Equipment equipment) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        String date = new SimpleDateFormat("EEE, dd MMM yyyy").format(equipment.getNextMaintenance());
+        String text = "A máquina portadora do número de série "
+        				+ equipment.getSerialNumber() 
+        				+ " tem sua próxima manutenção agendada para a data "
+        				+ date
+        				+ ".";
+        message.setTo(to); 
+        message.setSubject(subject); 
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
