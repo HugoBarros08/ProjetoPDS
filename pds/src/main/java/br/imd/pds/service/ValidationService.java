1145f987371773f63e04345d06de32b4b59c789b
Heytor Mesquita <heytormesquita@gmail.com>
2018-04-25 02:14:22 -0300

    Criado o método para validar email

--- pds/src/main/java/br/imd/pds/service/ValidationService.java
+++ pds/src/main/java/br/imd/pds/service/ValidationService.java
@@ -1,17 +1,32 @@
 package br.imd.pds.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Classe responsável por validar email e CPF
  */
 public class ValidationService {
 
-	public boolean ValidationCPF(String cpf) {
+	private static final String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private static final Pattern pattern = Pattern.compile(emailPattern, Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * Método para validar o email de um usuário
+	 * 
+	 * @param email string com email a ser verificado
+	 * @return true se o email é válido
+	 */
+	public boolean validationEmail(String email) {
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	
+	public boolean validationCPF(String cpf) {
 		
 		return true;
 	}
 	
-	public boolean ValidationEmail(String email) {
-		
-		return true;
-	}
+
 }
